7c937191744f91544880eb985e995d889e1301e8
Tyler Hughes <tylerhx111+git@proton.me>
2025-03-11 20:38:49 -0400

    !!! initial cesium widget

---
+++ src/qtcesium/__init__.py
@@ -0,0 +1,12 @@
+from __future__ import annotations
+
+try:
+    from ._version import __version__
+except ImportError:
+    __version__ = "0.0"
+
+from .cesium import QCesium
+
+__all__ = [
+    "QCesium",
+]
---
+++ src/qtcesium/cesium.py
@@ -0,0 +1,23 @@
+from __future__ import annotations
+
+import importlib.resources
+from PyQt6 import (
+    QtCore,
+    QtWebEngineCore,
+    QtWebEngineWidgets,
+)
+
+
+class QCesium(QtWebEngineWidgets.QWebEngineView):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        settings = [
+            QtWebEngineCore.QWebEngineSettings.WebAttribute.LocalContentCanAccessFileUrls,
+            QtWebEngineCore.QWebEngineSettings.WebAttribute.LocalContentCanAccessRemoteUrls,
+            QtWebEngineCore.QWebEngineSettings.WebAttribute.WebGLEnabled,
+        ]
+
+        for setting in settings:
+            self.settings().setAttribute(setting, True)
+
