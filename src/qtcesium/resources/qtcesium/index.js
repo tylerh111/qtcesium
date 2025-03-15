6867886fffe954ca82218818e53680df0c10c8f3
Tyler Hughes <tylerhx111+git@proton.me>
2025-03-15 00:36:36 -0400

    >>> fix export

--- src/qtcesium/resources/qtcesium/index.js
+++ src/qtcesium/resources/qtcesium/index.js
@@ -1,20 +1,12 @@
 
+export * as qcesium_channel from "@/qtcesium/qcesium_channel";
+export * as qcesium_debug from "@/qtcesium/qcesium_debug";
+export * as qcesium_entity from "@/qtcesium/qcesium_entity";
+export * as qcesium_init from "@/qtcesium/qcesium_init";
+export * as qcesium_viewer from "@/qtcesium/qcesium_viewer";
+
 export * from "@/qtcesium/qcesium_channel";
 export * from "@/qtcesium/qcesium_debug";
 export * from "@/qtcesium/qcesium_entity";
 export * from "@/qtcesium/qcesium_init";
 export * from "@/qtcesium/qcesium_viewer";
-
-import * as qcesium_channel from "@/qtcesium/qcesium_channel";
-import * as qcesium_debug from "@/qtcesium/qcesium_debug";
-import * as qcesium_entity from "@/qtcesium/qcesium_entity";
-import * as qcesium_init from "@/qtcesium/qcesium_init";
-import * as qcesium_viewer from "@/qtcesium/qcesium_viewer";
-
-export {
-    qcesium_channel,
-    qcesium_debug,
-    qcesium_entity,
-    qcesium_init,
-    qcesium_viewer,
-}
