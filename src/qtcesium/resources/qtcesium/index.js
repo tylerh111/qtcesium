6fd81243e60b2f85ab70605f13cc5e1705d5e823
Tyler Hughes <tylerhx111+git@proton.me>
2025-03-14 23:51:14 -0400

    moved initialization to separate module

--- src/qtcesium/resources/qtcesium/index.js
+++ src/qtcesium/resources/qtcesium/index.js
@@ -1,45 +1,20 @@
 
+export * from "@/qtcesium/qcesium_channel";
+export * from "@/qtcesium/qcesium_debug";
+export * from "@/qtcesium/qcesium_entity";
+export * from "@/qtcesium/qcesium_init";
+export * from "@/qtcesium/qcesium_viewer";
+
 import * as qcesium_channel from "@/qtcesium/qcesium_channel";
 import * as qcesium_debug from "@/qtcesium/qcesium_debug";
 import * as qcesium_entity from "@/qtcesium/qcesium_entity";
+import * as qcesium_init from "@/qtcesium/qcesium_init";
 import * as qcesium_viewer from "@/qtcesium/qcesium_viewer";
 
 export {
     qcesium_channel,
     qcesium_debug,
     qcesium_entity,
+    qcesium_init,
     qcesium_viewer,
-}
-
-/**
- * Connect remote signal to slot.
- *
- * @param {object} obj Channel object that is connected to the Qt application.
- * @param {(object, object) => any} fn Function to connect that takes `args` object followed by `opts` object.
- * @param {object} opts Function options.
- */
-function _connect(obj, fn, opts={}) {
-    obj[fn.name].connect((args) => {
-        fn(args, opts);
-    });
-}
-
-
-/**
- * Initialize QCesium application.
- *
- * @param {string} container Container where the cesium viewer is to be displayed.
- */
-export function initialize(container) {
-    qcesium_viewer.initialize_cesium_viewer(container, (viewer) => {
-        globalThis.app_viewer = viewer;
-    });
-
-    qcesium_channel.initialize_qwebchannel((channel) => {
-        globalThis.app_channel = channel;
-        globalThis.app_remote = channel.objects.remote;
-
-        _connect(channel.objects.remote, qcesium_entity.qcesium_create_cesium_entity, {viewer: globalThis.app_viewer});
-
-    });
 }
---
+++ src/qtcesium/resources/qtcesium/qcesium_init.js
@@ -0,0 +1,31 @@
+
+import * as QCesium from "@/qtcesium";
+
+/**
+ * Connect remote signal to slot.
+ *
+ * @param {object} obj Channel object that is connected to the Qt application.
+ * @param {(object, object) => any} fn Function to connect that takes `args` object followed by `opts` object.
+ * @param {object} opts Function options.
+ */
+function _connect(obj, fn, opts={}) {
+    obj[fn.name].connect((args) => {
+        fn(args, opts);
+    });
+}
+
+/**
+ * Initialize QCesium application.
+ *
+ * @param {string} container Container where the cesium viewer is to be displayed.
+ */
+export function initialize(container) {
+    QCesium.initialize_cesium_viewer(container, (viewer) => {
+        globalThis.app_viewer = viewer;
+    });
+
+    QCesium.initialize_qwebchannel((channel) => {
+        globalThis.app_channel = channel;
+        globalThis.app_remote = channel.objects.remote;
+    });
+}
