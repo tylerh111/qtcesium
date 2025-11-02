9e87aff97d429c45f9e5c10155717dbc557d1836
Tyler Hughes <tylerhx111+git@proton.me>
2025-11-01 20:40:26 -0400

    cleanup code from debugging

--- src/qtcesium/resources/qtcesium/qcesium_debug.js
+++ src/qtcesium/resources/qtcesium/qcesium_debug.js
@@ -9,32 +9,3 @@ export async function qcesium_run_debug (
 ) {
 
 }
-
-
-
-
-
-// /**
-//  * QCesium debug hook.
-//  */
-// export function qcesium_debug(
-//     {
-//         val="hello",
-//     }={},
-//     {
-//         viewer,
-//     }={},
-// ) {
-//     // switch between fixed and inertial
-//     console.error("::DEBUG::" + val);
-//     console.error("::DEBUG::" + viewer);
-
-//     if (viewer.referenceFrame === Cesium.ReferenceFrame.FIXED) {
-//         viewer.referenceFrame = Cesium.ReferenceFrame.INERTIAL;
-//     }
-//     else if (viewer.referenceFrame === Cesium.ReferenceFrame.INERTIAL) {
-//         viewer.referenceFrame = Cesium.ReferenceFrame.FIXED;
-//     }
-//     // console.error("::DEBUG::" + JSON.stringify(arguments, null, 4));
-// }
-
